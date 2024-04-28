deb41331c7b282630980a28fcd02069940dc675b
Louai99k <louaikelani99k@gmail.com>
2024-04-28 22:04:19 +0300

    remove snippets

--- .config/nvim/lua/plugins/configs/cmp.lua
+++ .config/nvim/lua/plugins/configs/cmp.lua
@@ -1,11 +1,6 @@
 local cmp = require("cmp")
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
-		end,
-	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -15,7 +10,6 @@ cmp.setup({
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		{ name = "vsnip" },
 	}, {
 		{ name = "buffer" },
 	}),
