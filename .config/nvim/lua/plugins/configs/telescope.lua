3739055b930668700841bf109473c989a9dd927c
Louai99k <louaikelani99k@gmail.com>
2024-05-07 22:56:43 +0300

    add utils to telescope

--- .config/nvim/lua/plugins/configs/telescope.lua
+++ .config/nvim/lua/plugins/configs/telescope.lua
@@ -1,19 +1,21 @@
-require("telescope").load_extension("live_grep_args")
-
+local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+local utils = require("utils.telescope")
+
+telescope.load_extension("live_grep_args")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
 vim.keymap.set(
 	"n",
 	"<leader>fs",
-	require("telescope").extensions.live_grep_args.live_grep_args,
+	telescope.extensions.live_grep_args.live_grep_args,
 	{ noremap = true, desc = "Find In Files" }
 )
-
 vim.keymap.set(
 	"n",
 	"<leader>fg",
-	":lua require('utils.telescope').grepAtCurrentTreeNode()<CR>",
-	{ desc = "Find On Tree Cursor" }
+	utils.grep_at_current_tree_node,
+	{ desc = "Find On Tree Cursor", silent = true, noremap = true }
 )
 vim.keymap.set("n", "<leader>fq", builtin.resume, { desc = "Resume Previous Telescope" })
+vim.keymap.set("n", "<leader>fe", utils.search_word_under_cursor, { desc = "Resume Previous Telescope" })
